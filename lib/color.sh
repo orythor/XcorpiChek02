418231e2b641deea41d0ef78b687bd8c48f04a14
XioNiV <maxomstore@gmail.com>
2026-09-21 09:18:00 +0700

    Create color.sh

---
+++ lib/color.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RESET="\e[0m"
+BOLD="\e[1m"
+DIM="\e[2m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+CYAN="\e[36m"
+MAGENTA="\e[35m"
+WHITE="\e[37m"
+
+info()    { echo -e "${CYAN}[*]${RESET} $*"; }
+success() { echo -e "${GREEN}[✓]${RESET} $*"; }
+warn()    { echo -e "${YELLOW}[!]${RESET} $*"; }
+error()   { echo -e "${RED}[✗]${RESET} $*"; }
+section() { echo -e "\n${CYAN}━━━ ${BOLD}$*${RESET}${CYAN} ━━━${RESET}"; }
